ca02c461f35de4d7d284658179d3e6876a7a9ef7
Forrest Stone <iloveburningman@gmail.com>
2017-10-12 15:24:46 -0700

    missing some stuff???

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -187,6 +187,9 @@ private
 
   def rows_to_array(rows)
     rows.map { |row| new(Hash[columns.zip(row)]) }
+    collection = BlocRecord::Collection.new
+    rows.each { |row| collection << new(Hash[columns.zip(row)]) }
+    collection
   end
 end
 
